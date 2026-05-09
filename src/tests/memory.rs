27831d320975c8066864ca20f9d4a27f014a6330
Robert Williamson <robert11122002@gmail.com>
2026-05-09 01:21:10 +0100

    remove vec!, add ldmia with writeback in test

--- src/tests/memory.rs
+++ src/tests/memory.rs
@@ -6,13 +6,13 @@ fn push_pop_range() {
     // cond AL = 0xE. Push base = 0x092D0000.
     // 0xE92D003F -> [0x3F, 0x00, 0x2D, 0xE9]
     let bytes = assemble("push {r0-r5}").unwrap();
-    assert_eq!(bytes, vec![0x3f, 0x00, 0x2d, 0xe9]);
+    assert_eq!(bytes, [0x3f, 0x00, 0x2d, 0xe9]);
 
     // pop {r1-r3, pc} -> r1, r2, r3, pc (15).
     // mask = 0b1000_0000_0000_1110 = 0x800E
     // Pop base = 0x08BD0000 -> 0xE8BD800E
     let bytes = assemble("pop {r1-r3, pc}").unwrap();
-    assert_eq!(bytes, vec![0x0e, 0x80, 0xbd, 0xe8]);
+    assert_eq!(bytes, [0x0e, 0x80, 0xbd, 0xe8]);
 }
 
 #[test]
@@ -20,27 +20,41 @@ fn ldm_stm() {
     let code = "ldm r0, {r1-r3}";
     let bytes = assemble(code).unwrap();
     // cond AL=0xE. base=0x08900000. Rn=0. mask=r1,r2,r3=0x000E. -> 0xE890000E
-    assert_eq!(bytes, vec![0x0e, 0x00, 0x90, 0xe8]);
+    assert_eq!(bytes, [0x0e, 0x00, 0x90, 0xe8]);
 
     let code = "stm r0, {r2, r4-r5}";
     let bytes = assemble(code).unwrap();
     // base=0x08800000. mask=r2,r4,r5=0x0034. -> 0xE8800034
-    assert_eq!(bytes, vec![0x34, 0x00, 0x80, 0xe8]);
+    assert_eq!(bytes, [0x34, 0x00, 0x80, 0xe8]);
 }
 
 #[test]
 fn ldm_stm_writeback() {
+    let code = "ldmia r1!, {r2-r4, pc, sp}";
+    let bytes = assemble(code).unwrap();
+    // cond AL=0xE. base=0x08900000. U=1, W=1, L=1 -> +0x00B00000. Rn = r1 -> +0x00010000
+    // register mask:
+    //   r2 = 0x0004
+    //   r3 = 0x0008
+    //   r4 = 0x0010
+    //   sp(r13) = 0x2000
+    //   pc(r15) = 0x8000
+    // total mask = 0xA01C
+    // final encoding:
+    // 0xE8B1A01C
+    assert_eq!(bytes, [0x1c, 0xa0, 0xb1, 0xe8]);
+
     let code = "stmia r0!, {r1-r8}";
     let bytes = assemble(code).unwrap();
     // cond AL=0xE. base=0x08800000. W=1 (0x00200000). Rn=0. mask=r1-r8=0x01FE. -> 0xE8A001FE
-    assert_eq!(bytes, vec![0xfe, 0x01, 0xa0, 0xe8]);
+    assert_eq!(bytes, [0xfe, 0x01, 0xa0, 0xe8]);
 }
 
 #[test]
 fn ldr_immediate_offset() {
     let code = "ldr r0, [r1, #4]";
     let bytes = assemble(code).unwrap();
-    assert_eq!(bytes, vec![0x04, 0x00, 0x91, 0xe5]);
+    assert_eq!(bytes, [0x04, 0x00, 0x91, 0xe5]);
 }
 
 #[test]
@@ -59,28 +73,28 @@ fn extra_load_store() {
 fn str_immediate_offset() {
     let code = "str r0, [r1, #8]";
     let bytes = assemble(code).unwrap();
-    assert_eq!(bytes, vec![0x08, 0x00, 0x81, 0xe5]);
+    assert_eq!(bytes, [0x08, 0x00, 0x81, 0xe5]);
 }
 
 #[test]
 fn ldrb_register_offset() {
     let code = "ldrb r0, [r1, r2]";
     let bytes = assemble(code).unwrap();
-    assert_eq!(bytes, vec![0x02, 0x00, 0xd1, 0xe7]);
+    assert_eq!(bytes, [0x02, 0x00, 0xd1, 0xe7]);
 }
 
 #[test]
 fn strb_scaled_offset() {
     let code = "strb r0,[r1, r2, lsl #2]";
     let bytes = assemble(code).unwrap();
-    assert_eq!(bytes, vec![0x02, 0x01, 0xc1, 0xe7]);
+    assert_eq!(bytes, [0x02, 0x01, 0xc1, 0xe7]);
 }
 
 #[test]
 fn ldr_pc_relative() {
     let code = "ldr r0, [pc, #8]";
     let bytes = assemble(code).unwrap();
-    assert_eq!(bytes, vec![0x08, 0x00, 0x9f, 0xe5]);
+    assert_eq!(bytes, [0x08, 0x00, 0x9f, 0xe5]);
 }
 
 #[test]
